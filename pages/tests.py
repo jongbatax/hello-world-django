1cfb8a8e0dd61449c15ee85d87877c990bfb3eb7
Herri Siagian <herry.it.2007@gmail.com>
2023-09-10 23:06:14 +0700

    "add test to pages"

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+"""test.py"""
 
-# Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+    """Test HomePageView url."""
+
+    def test_url_exists_at_correct_location(self):
+        """Test corect location at /"""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url reverse home"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        """Test correct template name"""
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "pages/home.html")
+
+    def test_template_content(self):
+        """Test template home contain spesific html code"""
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
+
+
+class AboutpageTests(SimpleTestCase):
+    """Test AboutPageView url."""
+
+    def test_url_exists_at_correct_location(self):
+        """Test corect location /about/"""
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url reverse about"""
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        """Test correct template name"""
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "pages/about.html")
+
+    def test_template_content(self):
+        """Test template about contain spesific html code"""
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
